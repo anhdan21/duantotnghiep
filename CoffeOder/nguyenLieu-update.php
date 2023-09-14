1648557524133e2c733fabe78cc5fa5941ba9e47
anhdan21 <109662308+anhdan21@users.noreply.github.com>
2023-09-14 15:40:55 +0700

    css

--- CoffeOder/nguyenLieu-update.php
+++ CoffeOder/nguyenLieu-update.php
@@ -104,10 +104,161 @@ if (isset($_POST['btnSave'])) {
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thêm Bàn</title>
+    <link rel="stylesheet" href="ThemBan.css">
+    <script src="https://kit.fontawesome.com/e123c1a84c.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="div-all">
+        <nav>
+            <section class="head">
+                <h2>Coffee Bee Order</h2>
+                <section class="use">
+                    <img src="./anh/use.png" class="img" alt="">
+                    <section>
+                        <span>Xin chào,</span> <br>
+                        <span>Administrator</span>
+                    </section>
+                </section>
+                <span>GENERAL</span>
+            </section>
+
+            <section class="menu">
+            <ul>
+                    <a href="../CoffeOder/danhMuc-get.php">
+                        <li><i class="fas fa-caravan"></i>Đồ bán chạy</li>
+                    </a>
+                    <a href="../man_chinh/Quan_ly_do_uong.html">
+                        <li><i class="fas fa-wine-glass-alt"></i>Quản lý đồ uống</li>
+                    </a>
+                    <a href="../man_chinh/Quan_ly_nguyen_lieu.html">
+                        <li><i class="fas fa-seedling"></i>Quản lý nguyên liệu</li>
+                    </a>
+                    <a href="../CoffeOder/ban-get.php">
+                        <li>Quản lý bàn </li>
+                    </a>
+                    <a href="../CoffeOder/user-get.php">
+                        <li>Tài khoản nhân viên</li>
+                    </a>
+                    <a href="../CoffeOder/hoaDonct-get.php">
+                        <li>Hóa đơn</li>
+                    </a>
+                    <a href="../man_chinh/Khuyen_mai.html">
+                        <li>Khuyến mại</li>
+                    </a>
+                </ul>
+            </section>
+        </nav>
+        <main>
+
+            <section class="canhan">
+                <i class="fas fa-bars"></i>
+                <img src="./anh/use.png" alt="">
+                <section class="dropdown">
+                    <section class="dropdwon_select">
+                        <span class="dropdown_selected"> Administrator</span>
+                        <i class="fas fa-sort-down"></i>
+                        <ul class="dropdown_list">
+                            <a href="http://127.0.0.1:5500/duantotnghiep/Dang_nhap/Doi_mat_khau.html"><li class="dropdown_item">
+                                <span class="dropdown_test"> Đổi Mật Khẩu</span>
+                                <i class="fas fa-key"></i>
+                            </li></a>
+                            <a href="http://127.0.0.1:5500/duantotnghiep/Dang_nhap/dang_nhap.html" type=" color: #000">
+                                <li class="dropdown_item">
+                                    <span class="dropdown_test">Đăng Xuất</span>
+                                    <i class="fas fa-sign-out-alt"></i>
+                                </li>
+                            </a>
+
+                        </ul>
+                    </section>
+
+                </section>
+            </section>
+            <section class="tenQL">
+                <a href="http://127.0.0.1:5500/duantotnghiep/man_chinh/Quan_ly_ban.html"><span>Quản lý bàn</span></a>
+                <a href="http://127.0.0.1:5500/duantotnghiep/Them/ThemBan.html"><span>/ Thêm bàn</span></a>
+                
+            </section>
+            <div id="container">
+        <p><?php echo implode('<br>', $err); ?></p>
+
+        <form action="" method="post" enctype="multipart/form-data">
+            <span>ID :<?php echo $row['Id_nguyenLieu']; ?></span>
+
+            <label for="id_User">Id người nhập</label>
+            <input type="text" name="id_User" value="<?php echo $row['id_User']; ?>">
+            
+            <label for="ten_nguyenLieu">Tên nguyên liệu</label>
+            <input type="text" name="ten_nguyenLieu" value="<?php echo $row['ten_nguyenLieu']; ?>">
+
+            <label for="soLuong">Số lượng</label>
+            <input type="text" name="soLuong" value="<?php echo $row['soLuong']; ?>">
+
+            <label for="price">Giá</label>
+            <input type="text" name="price" value="<?php echo $row['price']; ?>">
+
+            Select image to upload:
+                    <input type="file" name="img_nguyenLieu" id="img_nguyenLieu"><br>
+
+            <label for="ten_nguyenLieu">Kiểu dạng</label>
+            <input type="text" name="kieuNguyenLieu" value="<?php echo $row['kieuNguyenLieu']; ?>">
+
+            <button name="btnSave">Save Update</button>
+            <button name="btnCancel">Cancel</button>
+        </form>
+    </div>
+            
+        </main>
+    </div>
+
+</body>
+<script>
+    function getCurrentTime() {
+            // Tạo đối tượng Date đại diện cho thời gian hiện tại
+            const now = new Date();
+
+            // Lấy thông tin về giờ, phút và giây
+            const hour = now.getHours();
+            const minute = now.getMinutes();
+            const second = now.getSeconds();
+
+            // Định dạng lại chuỗi để hiển thị đẹp hơn (nếu muốn)
+            const formattedTime = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}:${second.toString().padStart(2, '0')}`;
+
+            // Hiển thị giờ hiện tại trong thẻ p có id="current-time"
+            document.getElementById('current-time').textContent = formattedTime;
+        }
+
+        // Gọi hàm getCurrentTime mỗi giây một lần để cập nhật giờ hiện tại
+        setInterval(getCurrentTime, 1000);
+        function getCurrentDate() {
+            // Tạo đối tượng Date đại diện cho thời gian hiện tại
+            const now = new Date();
+
+            // Lấy thông tin về ngày, tháng và năm
+            const day = now.getDate();
+            const month = now.getMonth() + 1; // Tháng trong JavaScript bắt đầu từ 0 (0 -> Tháng 1)
+            const year = now.getFullYear();
+
+            // Định dạng lại chuỗi để hiển thị đẹp hơn (nếu muốn)
+            const formattedDate = `${day.toString().padStart(2, '0')}/${month.toString().padStart(2, '0')}/${year}`;
+
+            // Hiển thị ngày hiện tại trong thẻ p có id="current-date"
+            document.getElementById('current-date').textContent = formattedDate;
+        }
+
+        // Gọi hàm getCurrentDate khi trang được tải và sau đó mỗi ngày một lần để cập nhật ngày hiện tại
+        getCurrentDate();
+</script>
+<style>
 #container {
     max-width: 50%;
     margin: 0 auto;
@@ -156,7 +307,219 @@ button[name="btnCancel"] {
     background-color: #ff0000; /* Màu đỏ */
 }
 
+body{
+    margin: auto;
+    padding: 0;
+    font-family: 'Inter', sans-serif;
+    width: 1900px;
+    height: 1080px;
+}
+@font-face {
+    font-family: 'Inter';
+    src: url('đường_dẫn_đến_tệp_font/inter.woff2') format('woff2'),
+         url('đường_dẫn_đến_tệp_font/inter.woff') format('woff');
+    /* Nếu muốn hỗ trợ thêm các định dạng font khác, bạn có thể thêm vào đây */
+    font-weight: normal; /* Trọng lượng phông chữ */
+    font-style: normal; /* Kiểu phông chữ */
+  }
+ 
+.div-all{
+    display: flex;
+}
+nav{
+    flex: 1;
+    background-color: #2A3F53;
+    color: aliceblue;
+    padding: 20px;
+
+}
+.head h2{
+    margin-left: 60px;
+    margin-bottom: 40px;
+}
+.img{
+    border: 1px solid #ffffff;
+    border-radius: 50%;
+    padding: 20px;
+    background-color: white;
+    flex-shrink: 0; /* Đảm bảo ảnh không bị co lại khi không đủ không gian */
+    width: 25px; /* Đặt chiều rộng ảnh là 200px */
+    height: auto;
+}
+.use{
+    display: flex;
+    row-gap: 1fr;
+    margin-bottom: 20px;
+    
+}
+/**/
+/*xin chao */
+.use section{
+    margin-left: 40px;
+    margin-top: 15px;
+}
+/*menu list danh sach */
+.menu {
+    margin-top: 40px;
+    border-top:  1px solid white;
+    padding-bottom: 90px;
+}
+.menu ul{
+    list-style-type: none;
+}
+ul li i{
+     margin-right: 20px;
+     width: 20px;
+     height: 10px;
+}
+ul li{
+    margin-top: 70px;
+}
+ul a{
+    color: white;
+        text-decoration: none;
+}
+main{
+
+    flex: 5;
+}
+/****************************************************/
+main i{
+
+}
+.canhan {
+    background-color: #D9D9D9;
+    padding: 25px;
+    position: relative;
+    
+}
+.canhan img{
+    position: absolute;
+    right: 190px;
+    margin-top: -12px;
+    border: 1px solid #ffffff;
+    border-radius: 50%;
+    padding: 15px;
+    background-color: white;
+    flex-shrink: 0; /* Đảm bảo ảnh không bị co lại khi không đủ không gian */
+    width: 15px; /* Đặt chiều rộng ảnh là 200px */
+    height: auto;
+}
+.canhan .dropdown{
+    position: absolute;
+    right: 60px;
+    margin-top: -20px;
+}
+.dropdown{
+   position: relative;
+}
+.dropdown_select{
+    cursor: pointer;
+   
+}
+.dropdown:hover .dropdown_list{
+    display: block;
+}
+.dropdown_selected {
+   
+}
+.dropdown_list{
+    width: 135px;
+    border-radius:  4px;
+    background-color: #D9D9D9;
+    position: absolute;
+    top: 100%;
+    left: 0;
+    right: 0;
+    display: none;
+}
+.dropdown_list::before{
+    content: "";
+    height: 20px;
+    position: absolute;
+    left: 0;
+    right: 0;
+    background-color: transparent;
+    transform: translateY(-100%);
+}
+.dropdown_item{
+   
+    text-align: center;
+    margin-top: -5px;
+    margin-left: -40px;
+    padding: 15px;
+    cursor: pointer;
+    transition: background-color 0.2s linear;
+    list-style-type: none;
+}
+.dropdown_item:hover{
+    background-color: #2A3F53;
+}
+/****************************************************/
+.tenQL{
+    margin-top: 10px;
+    padding: 25px;
+    border-bottom:1px solid #D9D9D9 ;
+}
+.tenQL a{
+    text-decoration: none;
+    color: black;
+}
+.thoigian{
+    padding: 20px;
+    border-bottom: 2px solid #D9D9D9;
+}
+.themDS{
+    position: absolute;
+    right: 50px;
+    width: 100px;
+}
+.themDS button{
+    padding: 5px;
+}
+/****************************************************/
+.thongtinMK{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    margin-top: 100px;
+}
+.thongtinMK label{
+    margin-left: -100px;
+    padding: 10px;
+
+}
+.thongtinMK input{
+    margin-left: 30px;
+    width: 500px;
+    padding: 10px;
+}
+.thongtinMK span{
+    margin-top: 20px;
+    margin-left: -300px;
+}
+.thongtinMK button{
+    padding: 10px;
+    margin: 50px 0 0 500px;
+    border-radius: 5px;
+}
+.thongtinMK button:hover{
+    background-color: #2A3F53;
+    color: white;
+}
+.oclock{
+    display: flex;
+    flex-direction: row-reverse;
+    justify-content: center;
+}
     </style>
+</html>
+<!-- <!DOCTYPE html>
+<html> -->
+
+<head>
+   
 </head>
 
 <body>
